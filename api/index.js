d3e5a0a7a6659c50b699b5fe3619259eaea8fd7f
Gabriel Aragão <117946090+gabrielfrz@users.noreply.github.com>
2025-05-02 23:27:17 +0000

    feat: add secure endpoint with JWT auth and example controller

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import db from './database/configdb.js';
 import User from './models/User.js';
 import userRoute from './routes/user.route.js';
+import exemploeroute from './routes/example.route.js';
 
 dotenv.config();
 db.connect();
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/user", userRoute);
+app.use("/secureExampleRoute", exemploeroute);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World!' });
---
+++ example_requests/GET_Secure_Endpoint.sh
@@ -0,0 +1,3 @@
+curl --request GET \
+  --url 'http://localhost:3000/secureExampleRoute/' \
+  --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4MTU0YTEwZjRlZDAwNTMwZDRmOTMyZCIsImlhdCI6MTc0NjIyODI2NSwiZXhwIjoxNzQ2MjMxODY1fQ.mlwjv4Djb7hlIq-CACO3A1j7Xpp4IKbg1Da8R-f8kKM'
